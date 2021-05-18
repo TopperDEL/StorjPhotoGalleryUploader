ee68d552d640fb606a5e2841da2e15030ba014fc
TopperDEL <github_com@tparth.de>
2021-05-18 14:18:34 +0200

    feat: IsLoading and NavigateNewAlbum-Command

--- StorjPhotoGalleryUploader.Shared/ViewModels/AlbumListViewModel.cs
+++ StorjPhotoGalleryUploader.Shared/ViewModels/AlbumListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MvvmGen;
+using MvvmGen.Events;
 using StorjPhotoGalleryUploader.Contracts.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -8,23 +9,40 @@ using System.Threading.Tasks;
 
 namespace StorjPhotoGalleryUploader.ViewModels
 {
+    [Inject(typeof(IEventAggregator))]
     [Inject(typeof(IAlbumService))]
     [ViewModel]
     public partial class AlbumListViewModel
     {
-        [Property] private ObservableCollection<AlbumViewModel> _albumList = new ObservableCollection<AlbumViewModel>(); 
+        [Property] private ObservableCollection<AlbumViewModel> _albumList = new ObservableCollection<AlbumViewModel>();
+        [Property] private bool _isLoading;
 
         [Command]
         private async Task LoadAlbumsAsync()
         {
-            var albums = await AlbumService.ListAlbumsAsync();
-            foreach(var album in albums)
-            {
-                AlbumList.Add(new AlbumViewModel(album));
-            }
+            IsLoading = true;
 
-            AlbumList.Add(new AlbumViewModel(new Contracts.Models.Album() { Name = "Holiday Spain 2021" }));
-            AlbumList.Add(new AlbumViewModel(new Contracts.Models.Album() { Name = "Holiday Green 2020" }));
+            try
+            {
+                var albums = await AlbumService.ListAlbumsAsync();
+                foreach (var album in albums)
+                {
+                    AlbumList.Add(new AlbumViewModel(album));
+                }
+
+                AlbumList.Add(new AlbumViewModel(new Contracts.Models.Album() { Name = "Holiday Spain 2021" }));
+                AlbumList.Add(new AlbumViewModel(new Contracts.Models.Album() { Name = "Holiday Green 2020" }));
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        [Command]
+        private void NavigateNewAlbum()
+        {
+
         }
     }
 }
