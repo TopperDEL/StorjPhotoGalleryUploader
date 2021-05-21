24b38718b15cb8bd52122370eaf6e3de44f837e9
TopperDEL <github_com@tparth.de>
2021-05-21 21:24:12 +0200

    feat: Prepare bucket WIP, include site-template assets

--- StorjPhotoGalleryUploader.Services/PrepareBucketService.cs
+++ StorjPhotoGalleryUploader.Services/PrepareBucketService.cs
@@ -1,8 +1,11 @@
 ﻿using StorjPhotoGalleryUploader.Contracts.Models;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using uplink.NET.Interfaces;
 
 namespace StorjPhotoGalleryUploader.Services
 {
@@ -10,23 +13,49 @@ namespace StorjPhotoGalleryUploader.Services
     {
         public event PreparationStateChangedEventHandler PreparationStateChangedEvent;
 
+        private IEnumerable<string> _assetNames;
+
+        readonly IObjectService _objectService;
+        readonly IBucketService _bucketService;
+
+        public PrepareBucketService(IObjectService objectService, IBucketService bucketService)
+        {
+            _objectService = objectService;
+            _bucketService = bucketService;
+        }
+
+        private void Init()
+        {
+            if (_assetNames == null)
+            {
+                Assembly assembly = GetType().GetTypeInfo().Assembly;
+                _assetNames = assembly.GetManifestResourceNames().Where(n => n.Contains("StorjPhotoGalleryUploader.Services.Assets.site_template") && !n.Contains("index.html"));
+            }
+        }
+
         public async Task<bool> CheckIfBucketNeedsPrepareAsync()
         {
-            await Task.Delay(2000);
+            Init();
+
+            //ToDo: Check if every file exists in the bucket
+            await Task.Delay(100);
             return false;
         }
 
         public async Task<BucketPrepareResult> PrepareBucketAsync()
         {
-            await Task.Delay(1000);
+            Init();   
 
-            for (int i = 0; i <10; i++)
+            int current = 0;
+            foreach(var name in _assetNames)
             {
-                PreparationStateChangedEvent?.Invoke(i, 10, "Current step is " + i.ToString());
-                await Task.Delay(100);
+                var fileName = name.Replace("StorjPhotoGalleryUploader.Services.Assets.site_template.", "");
+                PreparationStateChangedEvent?.Invoke(current, _assetNames.Count(), fileName);
+                await Task.Delay(10); //ToDo: Upload file, respect correct filename
+                current++;
             }
 
-            return new BucketPrepareResult() { Successfull = false, PrepareErrorMessage ="This is a long and longer info about what might have happened. So show it to the user then..." };
+            return new BucketPrepareResult() { Successfull = false, PrepareErrorMessage = "This is a long and longer info about what might have happened. So show it to the user then..." };
         }
     }
 }
