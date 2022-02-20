951aef2dc113917a85a398155a24f15968a1ff33
TopperDEL <github_com@tparth.de>
2022-02-20 20:36:58 +0100

    feat: Raise error if no attachment could be selected

--- StorjPhotoGalleryUploader.Uno.Services/AttachmentSelectService.cs
+++ StorjPhotoGalleryUploader.Uno.Services/AttachmentSelectService.cs
@@ -1,4 +1,6 @@
-﻿using SkiaSharp;
+﻿using MvvmGen;
+using SkiaSharp;
+using StorjPhotoGalleryUploader.Contracts.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,30 +15,46 @@ namespace StorjPhotoGalleryUploader.Services
 {
     public class AttachmentSelectService : IAttachmentSelectService
     {
+        readonly IDialogService _dialogService;
+        readonly ILocalizedTextService _localizedTextService;
+
+        public AttachmentSelectService(IDialogService dialogService, ILocalizedTextService localizedTextService)
+        {
+            _dialogService = dialogService;
+            _localizedTextService = localizedTextService;
+        }
+
         public async Task<List<Attachment>> GetAttachmentsAsync()
         {
             List<Attachment> result = new List<Attachment>();
 
-            var fileOpenPicker = new FileOpenPicker();
-            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
-            fileOpenPicker.FileTypeFilter.Add(".jpg");
-            fileOpenPicker.FileTypeFilter.Add(".png");
-            var pickedFiles = await fileOpenPicker.PickMultipleFilesAsync();
-            if (pickedFiles.Count > 0)
+            try
             {
-                // At least one file was picked, you can use them
-                foreach (var file in pickedFiles)
+                var fileOpenPicker = new FileOpenPicker();
+                fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                fileOpenPicker.FileTypeFilter.Add(".jpg");
+                fileOpenPicker.FileTypeFilter.Add(".png");
+                var pickedFiles = await fileOpenPicker.PickMultipleFilesAsync();
+                if (pickedFiles.Count > 0)
                 {
-                    var attachment = new StorageFileAttachment();
-                    attachment.Filename = file.Name;
-                    attachment.MimeType = file.ContentType;
-                    attachment.StorageFile = file;
-                    result.Add(attachment);
+                    // At least one file was picked, you can use them
+                    foreach (var file in pickedFiles)
+                    {
+                        var attachment = new StorageFileAttachment();
+                        attachment.Filename = file.Name;
+                        attachment.MimeType = file.ContentType;
+                        attachment.StorageFile = file;
+                        result.Add(attachment);
+                    }
+                }
+                else
+                {
+                    // No file was picked or the dialog was cancelled.
                 }
             }
-            else
+            catch (Exception ex)
             {
-                // No file was picked or the dialog was cancelled.
+                await _dialogService.ShowErrorMessageAsync(ex.Message, _localizedTextService.GetLocalizedText("ErrorTitle"));
             }
 
             return result;
